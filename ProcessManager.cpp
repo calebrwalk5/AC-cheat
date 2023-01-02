06f9ec244e18f01d9fc3f7a44bc98f329066a6c0
zoomer <51061101+calebrwalk5@users.noreply.github.com>
2023-01-01 21:20:03 -0500

    Add files via upload

--- ProcessManager.cpp
+++ ProcessManager.cpp
@@ -1,5 +1,60 @@
-/* Getting intimate with memory */
 #include "ProcessManager.h"
+#include <dirent.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdint.h>
+
+ProcessManager::ProcessManager(unsigned long ProcessID, const char *module) {
+    printf("Here we go\n");
+
+    char FilePath[2048];
+
+    struct dirent *DirectoryObject = NULL;
+    DIR *DirectoryHandle = NULL;
+
+    if((DirectoryHandle = opendir("/proc/")) == NULL) {
+        printf("ERROR on line 12 of ProcessManager.cpp\n");
+        return;
+    }
+
+    while((DirectoryObject = readdir(DirectoryHandle)) != NULL) {
+        if(atoi(DirectoryObject->d_name) != 0) {
+            char FilePath[2048];
+            sprintf(FilePath, "/proc/%s/comm", DirectoryObject->d_name);
+
+            char ProcessName[2048];
+            int fd = 0;
+
+            if((fd = open(FilePath, O_RDONLY)) < 0) {
+                printf("Failed to open file\n");
+                return;
+            }
+
+            read(fd, ProcessName, 2048);
+            close(fd);
+
+            if(strcmp(ProcessName, module) == 0) {
+                ProcessID = atoi(DirectoryObject->d_name);
+                break;
+            }
+        }
+    }
+
+    closedir(DirectoryHandle);
+
+    sprintf(FilePath, "/proc/%lu/mem", ProcessID);
+
+    if((ProcessHandle = open(FilePath, O_RDWR)) < 0) {
+        printf("Failed to open process\n");
+        return;
+    }
+
+    TargetBaseAddress = FindBaseAddress(module);
+}
 
 long ProcessManager::FindBaseAddress(const char *module) {
     int fd = 0;
@@ -9,60 +64,54 @@ long ProcessManager::FindBaseAddress(const char *module) {
 
     sprintf(FileLocation, "/proc/%lu/maps", ProcessID);
 
-    if((fd = open(FileLocation, 0_RDONLY)) < 0) {
-        fprintf("Failed to open file\n");
-        return 1;
+    if((fd = open(FileLocation, O_RDONLY)) < 0) {
+        printf("Failed to open file\n");
+        return 0;
     }
 
     char *FileBuffer = (char *)malloc(500000);
     if(FileBuffer == NULL) {
-        fprintf("Malloc failed on line 16 of ProcessManager.cpp\n");
-        return 1;
+        printf("Malloc failed on line 16 of ProcessManager.cpp\n");
+        return 0;
     }
 
     memset(FileBuffer, 0, 500000);
     memset(BaseAddress, 0, 2048);
 
     for(int i = 0; read(fd, FileBuffer + i, 1) > 0; i++) {
-        fprintf("reading from file\n");
+        printf("reading from file\n");
     }
 
     close(fd);
-    fprintf("Closed FD");
+    printf("Closed FD");
 
     if(module != NULL) {
         if((ptr = strstr(FileBuffer, module)) == NULL) {
-            fprintf("Failed to find module\n");
-            return 1;
-        }
-    } else {
-        if((ptr = strstr(FileBuffer, "r-xp")) == NULL) {
-            fprintf("Failed to find memory module\n");
-            return 1;
-        }
+            printf("Failed to find module\n");
+            return 0;
     }
 
-    while(*ptr != "\n" && ptr >= FileBuffer) {
+    while(*ptr != '\n' && ptr >= FileBuffer) {
         ptr--;
     }
     ptr++;
 
-    for(int i = 0; *ptr != "-"; i++) {
+    for(int i = 0; strcmp(ptr, "-") != 0; i++) {
         BaseAddress[i] = *ptr;
         ptr++;
     }
 
     free(FileBuffer);
-    fprintf("Freed FB\n");
+    printf("Freed FB\n");
 
     return strtol(BaseAddress, NULL, 16);
 }
 
-bool ProcessManager::WriteProcessMemory(unsigned long address, void, *buffer, uint size) {
+bool ProcessManager::WriteProcessMemory(unsigned long address, void *buffer, uint size) {
     lseek(ProcessHandle, address, SEEK_SET);
 
-    if(!write(ProcessHandle, buffer, size)) {
-        fprintf("Error writing to memory\n");
+    if(write(ProcessHandle, buffer, size) != size) {
+        printf("Error writing to memory\n");
         return false;
     }
 
@@ -71,11 +120,11 @@ bool ProcessManager::WriteProcessMemory(unsigned long address, void, *buffer, ui
     return true;
 }
 
-bool ProcessManager::ReadProcessMemory(unsigned long address, void, *buffer, uint size) {
+bool ProcessManager::ReadProcessMemory(unsigned long address, void *buffer, uint size) {
     lseek(ProcessHandle, address, SEEK_SET);
 
-    if(!read(ProcessHandle, buffer, size)) {
-        fprintf("Error reading target memory\n");
+    if(read(ProcessHandle, buffer, size) != size) {
+        printf("Error reading target memory\n");
         return false;
     }
 
@@ -84,95 +133,21 @@ bool ProcessManager::ReadProcessMemory(unsigned long address, void, *buffer, uin
     return true;
 }
 
-bool ProcessManager::SignaturePayload(const char *signature char *payload, const int siglen, const int paylen, const int bsize, uint sigoffset = 0) {
+bool ProcessManager::SignaturePayload(const char *signature, char *payload, const int siglen, const int paylen, const int bsize, uint sigoffset) {    
     char *buf = (char *)malloc(siglen * bsize);
     
-    for(int i = 0; ReadProcessMemory(TargetBaseAddress + i, buf, siglen * bsize); i+= (siglen * bsize)) {
+    for(int i = 0; ReadProcessMemory(TargetBaseAddress + i, buf, siglen * bsize); i += (siglen * bsize)) {
         for(int x = 0; x < ((siglen * bsize) - (siglen - 1)); x++) {
-            if(memcmp(buf + x, signature, siglen) == 0) {
-                fprintf("Sig Found\n");
+            printf("Sig Found\n");
 
-                WriteProcessMemory((TargetBaseAddress + i + x + sigoffset, payload, paylen));
+            WriteProcessMemory((TargetBaseAddress + i + x + sigoffset), payload, paylen);
 
-                goto END; // hahahaha
-            }
+            break;
         }
     }
 
-END:
     free(buf);
 
     return true;
 }
-
-ProcessManager::ProcessManager(const char *szProcessName, const char *module) {
-    fprintf("Here we go\n");
-
-    strcpy(ProcessNameString, szProcessName);
-
-    struct dirent *DirectoryObject = NULL;
-    DIR *DirectoryHandle = NULL;
-
-    if((DirectoryHandle = opendir("/proc/")) == NULL) {
-        fprintf("ERROR on line 12 of ProcessManager.cpp\n");
-        return 1;
-    }
-
-    while((DirectoryObject = readdir(DirectoryHandle)) != NULL) {
-        if(atoi(DirectoryObject -> dname) != 0) {
-            char FilePath[2048];
-            char *FileBuffer = NULL;
-            __off_t FileLength = 256;
-            int fd = 0;
-
-            sprintf(FilePath, "proc/%s/status", DirectoryObject -> d_name);
-            fprintf("Reading /proc\n");
-
-            if((FileBuffer = (char *)malloc(FileLength)) == NULL) {
-                fprintf("Malloc failure on line 54 of ProcessManager.cpp\n");
-                return 1;
-            }
-
-            memset(FileBuffer, 0, FileLength);
-
-            if(read(fd, FileBuffer, FileLength) < 0) {
-                fprintf("Failed to read file\n")
-                return 1;
-            }
-
-            close(fd);
-            fprintf("Closed FD\n");
-
-            if(strstr(FileBuffer, ProcessNameString) != NULL) {
-                fprintf("Process probably found\n");
-
-                ProcessID = atol(DirectoryObject -> d_name);
-
-                char TargerMemoryLocation[2048];
-                sprintf(TargerMemoryLocation, "/proc/%s/mem", DirectoryObject -> d_name);
-
-                // get program base addr AHHHHHHHHHHHHHH!!!!!!!!
-                TargetBaseAddress = FindBaseAddress(module);
-
-                if((ProcessHandle = open(TargerMemoryLocation, 0_RDWR)) < 0) {
-                    fprintf("Failed to open target memory location\n");
-                    return 1;
-                }
-
-                free(FileBuffer);
-                fprintf("Freed FB\n");
-                break;
-            }
-            free(FileBuffer);
-            fprintf("Freed FB\n");
-        }
-    }
-    closedir(DirectoryHandle);
-}
-
-// real complex
-ProcessManager::~ProcessManager() {
-    if(ProcessHandle != 0) {
-        close(ProcessHandle);
-    }
 }
--- ProcessManager.h
+++ ProcessManager.h
@@ -7,7 +7,7 @@
 #include <strings.h>
 #include <unistd.h>
 #include <sys/stat.h>
-#include <sys/types.h
+#include <sys/types.h>
 #include <fcntl.h>
 #include <dirent.h>
 #include <errno.h>
@@ -32,11 +32,13 @@ class ProcessManager {
 
     // ProcessManager is a constructor that takes a char pointer and an optional char pointer as arguments
     ProcessManager(const char *szProcessName, const char *module = NULL);
+    // ProcessManager is a second constructor that takes an unsigned long and a char pointer as arguments
+    ProcessManager(unsigned long ProcessID, const char *module);
     // ProcessManager is a destructor
     ~ProcessManager();
 
     // SignaturePayload is a member function that takes a char pointer, a char pointer, two ints, an int, and an optional uint as arguments and returns a bool
-    bool SignaturePayload(const char *signature, char *payload, const int siglen, const int paylen, const int bsize, uint sigoffset = 0);
+    bool SignaturePayload(const char *signature, char *payload, const int siglen, const int paylen, const int bsize, uint sigoffset);
     
     // WriteProcessMemory is a member function that takes an unsigned long, a void pointer, and a uint as arguments and returns a bool
     bool WriteProcessMemory(unsigned long address, void *buffer, uint size);
